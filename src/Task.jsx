d70dbbe97ed9390bdd455328c068024e80ddfd59
Cody <c.jmiller17@yahoo.com>
2024-05-03 01:45:52 -0400

    Saving

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import moment from 'moment';
 import Button from "./Button";
 
-function Task({task, deleteTask, moveTaskUp, moveTaskDown}) {
+function Task({ task, deleteTask, moveTaskUp, moveTaskDown }) {
+    // Calc the diff between current date and due date
+    const dueDateDiff = moment(task.dueDate, "YYYY-MM-DD").diff(moment(), "days");
+    const dueDateTest = moment(task.dueDate, "YYYY-MM-DD");
+    console.log("Moment: ", dueDateTest)
+    console.log("Due Date Diff: ", dueDateDiff);
+    console.log(task.dueDate)
+
+    let dotColor = "";
+    if (dueDateDiff >= "5") {
+      dotColor = "green";
+    } else if (dueDateDiff >= 2) {
+      dotColor = "orange";
+    } else if (dueDateDiff >= 0) {
+      dotColor = "red";
+    } else {
+      dotColor = "lightgray";
+    }
 
     return (
       <>
         {/* {tasks.map((task, index) => ( */}
         <div key={task.id}>
-          <span className="status"> dot </span>
+          <span className="status" style={{ backgroundColor: dotColor }}> ** </span>
           <span className="text">{task.description}</span>
           {/* // This creates a condition that prevents 'due:'' from appearing on tasks without a due date
           {task.dueDate && (
